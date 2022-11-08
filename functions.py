0abd9319dd0863b77bdc4dd9aedf8e4e412ba8e7
sedron <jfabianabadd@gmail.com>
2022-11-07 20:07:33 -0500

    Added notes functions

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 from db import connect
 
+
 def register_user(id, email):
     conn = connect()
     with conn.cursor() as cursor:
@@ -7,12 +8,40 @@ def register_user(id, email):
     conn.commit()
     conn.close
 
+
 def get_id(email):
     conn = connect()
     with conn.cursor() as cursor:
         cursor.execute("SELECT * FROM USERS WHERE EMAIL = %s", (email))
         res = cursor.fetchone()
+    conn.close()
     if res:
         return res[0]
     else:
         return False
+
+
+def register_note(id, user_id, name):
+    conn = connect()
+    with conn.cursor() as cursor:
+        cursor.execute("INSERT INTO NOTES (CONTAINER_ID, USER_ID, NAME, CONTENT) VALUES (%s, %s, %s, %s)", (id, user_id, name, ''))
+    conn.commit()
+    conn.close()
+
+
+def get_notes(user_id):
+    conn = connect()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT CONTAINER_ID, NAME FROM NOTES WHERE USER_ID = %s", (user_id))
+        res = cursor.fetchall()
+    conn.close()
+    return res
+
+
+def modify_note(note_id, new_value):
+    conn = connect()
+    with conn.cursor() as cursor:
+        cursor.execute("UPDATE NOTES SET CONTENT = %s WHERE CONTAINER_ID = %s", (new_value, note_id))
+    conn.commit()
+    conn.close()
+    
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from uuid import uuid1
 
 from fastapi import FastAPI, Form, status
+from fastapi.encoders import jsonable_encoder
 from pydantic import EmailStr
 
-from functions import register_user, get_id
+from functions import *
 
 app = FastAPI()
 
-@app.get('/inicio')
+
+@app.get('/')
 async def ruta_de_prueba():
     return 'Hola desde FastAPI.'
+
 
 @app.post(
     path="/signup",
@@ -19,12 +22,54 @@ async def ruta_de_prueba():
 )
 async def signup(email: EmailStr = Form() ):
     user_id = get_id(email)
-    print(user_id)
     if not(user_id):
         user_id = uuid1()
         register_user(user_id, email)
     
     return user_id
+
+
+@app.post(
+    path="/create_note",
+    status_code=status.HTTP_201_CREATED,
+    summary="Create a Note in the Database",
+    tags=['Create']
+)
+async def add_note(user_id: str = Form(), name: str = Form()):
+    note_id = uuid1()
+    print(note_id)
+    register_note(note_id, user_id, name)
+    return note_id
+
+
+@app.get(
+    path='/get_containers/{user_id}',
+    status_code=status.HTTP_202_ACCEPTED,
+    summary="Get all the containers of the user",
+    tags=['Get']
+)
+async def get_containers(user_id: str):
+    # Returns an Array of arrays -> [ [container_id, name] ]
+    containers = []
+
+    # For the notes
+    for note in get_notes(user_id):
+        containers.append(note)
+
+    return jsonable_encoder(containers)
+
+
+@app.post(
+    path='/modify_note',
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Change the content of the note",
+    tags=['Modify']
+)
+async def change_note(note_id: str = Form(), new_value: str = Form()):
+    modify_note(note_id, new_value)
+     
+    return '200'
+
 
 
  
