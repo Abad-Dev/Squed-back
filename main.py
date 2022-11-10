a8db169f10b1ac0d0c77b8ee8ea89ad037e3f490
sedron <jfabianabadd@gmail.com>
2022-11-09 22:18:20 -0500

    Added CORS

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ from uuid import uuid1
 
 from fastapi import FastAPI, Form, status
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import EmailStr
 
 from functions import *
 
+
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
